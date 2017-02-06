83064cbd178391bbeb299ce0478084494c054ee8
Drew Carpenter <drew@plexserver.elcarpenter.com>
2017-02-06 15:59:53 -0500

    Converted the getlight() over to JSON and stored the original values into a variable before we start changing light levels

--- HaikuLightControlPlexServer.py
+++ HaikuLightControlPlexServer.py
@@ -11,8 +11,11 @@ from sensemefan import SenseMeFan
 
 
 # Global values
+	# ip_addr = IP Address, name = Name given to device, model = model of device, series = series of device
 haiku_data = namedtuple("HaikuData", "ip_addr name model series")
 room1_uuid = '81ba6448-c094-4da2-8cc3-6aa4d003764b'
+room1_light_status = "OFF"
+room1_light_level = 0
 
 living_room = haiku_data(ip_addr = "10.10.1.117", name = "Drew\'s Room Fan", model = "FAN", series = "LSERIES")
 # Statically assign the fan? Probably not, but you would do it this way:
@@ -51,9 +54,17 @@ def add_message(uuid):
   
    # Dim the lights if media is playing or resuming
    if ( ( event == 'media.play' ) or ( event == 'media.resume' ) ) and ( player_uuid == room1_uuid ) :
-      # Get Light level
-      print "Dimming Lights"
-      light = fan.getlight()
+      # Get Light level before diming
+	  #print fan.getlight()
+	  # Converts single quotes to double quotes to make it proper JSON
+	  print "Getting light status/level"
+	  temp_json = json.dumps(fan.getlight())
+	  temp_json = json.loads(temp_json)
+	  #print temp_json["brightness"]
+	  # Store the original values in variables before we start changing stuff
+	  room1_light_level = temp_json["brightness"]
+	  room1_light_status = temp_json["status"]
+	  print "Dimming Lights"
 	
    # Resume the lights if media is stopped or paused
    if ( ( event == 'media.stop' ) or ( event == 'media.pause' ) ) and ( player_uuid == room1_uuid ) :
