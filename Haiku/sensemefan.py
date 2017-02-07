ee4117a19427a7696509c60b1bb88b0bbfb994b2
Drew Carpenter <drew@plexserver.elcarpenter.com>
2017-02-07 14:33:18 -0500

    Now detects multiple devices (have only tested it with 2). It does not always detect my L Series fan and I am not sure why. The info for each fan is currently stored in a dictionary but is not accessable outside of the class yet

--- Haiku/sensemefan.py
+++ Haiku/sensemefan.py
@@ -185,30 +185,65 @@ class SenseMeFan:
             #print(m)
 
     def discover(self):
-        data = '<ALL;DEVICE;ID;GET>'.encode('utf-8')
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind(('', 0))
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		data = '<ALL;DEVICE;ID;GET>'.encode('utf-8')
 
-        res = s.sendto(data, ('<broadcast>', self.PORT))
-        print(res)
-        print('sent broadcast')
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.bind(('', 0))
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+		
+		res = s.sendto(data, ('<broadcast>', self.PORT))
+		#print(res)
+		#print('sent broadcast')
 
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.bind(('', 31415))
-        m = s.recvfrom(1048)
-        print(m)
-        if not m:
-            pass
-        else:
-            self.details = m[0].decode('utf-8') 
-            res = re.match('\((.*);DEVICE;ID;(.*);(.*),(.*)\)',self.details)
-            self.name = res.group(1)
-            self.mac = res.group(2)
-            self.model = res.group(3)
-            self.series = res.group(4)
-            self.ip = m[1][0]
-            # print('ip: ' + self.ip)
-            # print('details: ' + self.details)
-            # print(self.name)
-        pass
+		p = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		p.setblocking(0)
+		p.settimeout(5.0)
+		killWhile = False
+		i = 0
+		socket_data = []
+		device_list = {}
+		try:
+			p.bind(('', 31415))
+		except:
+			print 'Failed To Bind To Socket'
+			p.close()
+			raise
+			p.blocking(0)
+		while killWhile != True:
+			try:
+				m = p.recvfrom(2048)
+				if m:
+					print 'Data recevied'
+					socket_data.append(m)
+			except socket.timeout:
+				killWhile = True
+		if not socket_data[0]:
+			p.close()	# Closes the open socket
+			pass
+		else:
+			
+			# Interate through the entire list to parse out the info into a dict
+			for i  in range (0, len(socket_data)):										# Loop until it reaches the end of the list
+				m = socket_data[i]														# Put the data into a working variable
+				self.details = m[0].decode('utf-8') 									# Decode/Convert to UTF-8
+				res = re.match('\((.*);DEVICE;ID;(.*);(.*),(.*)\)',self.details)		# Regular expression to parse the info
+				self.name = res.group(1)												# Pull the name of the device out of the reg expression
+				self.mac = res.group(2)													# Pull the MAC ID of the device out of the reg expression
+				self.model = res.group(3)												# Pull the model of the device out of the reg expression
+				self.series = res.group(4)												# Pull the series of the device out of the reg expression
+				self.ip = m[1][0]														# Pull the IP of the device from the 2nd half of the response
+			
+				# Start storing the info into a dict
+				device_list[self.name] = self.name
+				device_list[self.name] = {}												# Needed to initalize a new dict inside of a dict
+				device_list[self.name]["MAC"] = self.mac
+				device_list[self.name]["Model"] = self.model
+				device_list[self.name]["Series"] = self.series
+				device_list[self.name]["IP"] = self.ip
+			print device_list
+			# Store all the data in a list
+			p.close()	# Closes the open socket
+			#print('ip: ' + self.ip)
+			#print('details: ' + self.details)
+			#print(self.name)
+			pass
