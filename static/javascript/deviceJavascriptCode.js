ec78d6863e0c17b26a3a0a423f6d56c904ed601d
Drew Carpenter <dcplaya@gmail.com>
2017-03-22 13:22:31 -0400

    Table data from DB implemented
    Device table now pulls data from database instead of querying.
    There is an option to query and update the database with new devices
    If a device is already in the table (based off of MAC address as key),
    the remaining values in the row are updated in the database
    Clicking on a row queries the device and enables links to control it
    (Fan right now does not work, a bug was introduced and I havent fixed
    it)

--- static/javascript/deviceJavascriptCode.js
+++ static/javascript/deviceJavascriptCode.js
@@ -5,13 +5,12 @@ function deviceClickedInTable(name, ip, model, series, mac) {
 
     // Start the status update request
     deviceStatusUpdateStart(name, ip, model, series, mac);
-};
+}
 
 // Start requesting device status updates and set the onClick for the links
 function deviceStatusUpdateStart(name, ip, model, series, mac) {
 
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","Model":"' + model + '","Series":"' + series + '","MAC":"' + mac + '"}');
-    
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","Model":"' + model + '","Series":"' + series + '","MAC":"' + mac + '"}');
     window.clearInterval(window.refreshIntervalID);                             // Stops the previous setInterval so I dont spam status update requests
     
     // Sends the command every X seconds
@@ -35,7 +34,7 @@ function deviceStatusUpdateStart(name, ip, model, series, mac) {
    
    // Return refreshID so I can kill it later on properly
    return refreshIntervalID;  
-};
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //These functions will be run whenever a device control link is clicked
@@ -49,59 +48,59 @@ function deviceLightToggle(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"TOGGLE", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"TOGGLE", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
     console.log("Device Light On String: ", client_message);
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceLightOn(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"ON", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"ON", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
     console.log("Device Light On String: ", client_message);
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceLightOff(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"OFF", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"OFF", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
     console.log("Device Light On String: ", client_message);
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceLightIncrease(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"INCREASE", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"INCREASE", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceLightDecrease(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"DECREASE", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"DECREASE", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":""' + '}}');
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 ////////////////////////////////////////////////////////////////////////////////
 //                          End light control functions
 ////////////////////////////////////////////////////////////////////////////////
@@ -113,59 +112,59 @@ function deviceFanToggle(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"TOGGLE", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"TOGGLE", "Speed":""' + '}}');
     //console.log("Device Fan On String: ", client_message);
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceFanOn(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"ON", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"ON", "Speed":""' + '}}');
     //console.log("Device Fan On String: ", client_message);
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceFanOff(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"OFF", "Speed":""' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"OFF", "Speed":""' + '}}');
     //console.log("Device Fan On String: ", client_message);
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceFanIncrease(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":"INCREASE"' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":"INCREASE"' + '}}');
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 function deviceFanDecrease(name, ip, model, series, mac) {
 
     // Standard JSON format to control devices will be
     // {MAC: mac, Model: model, Series: series, Light: { Power: on/off/toggle, Brightness: 0-16/INCREASE/DECREASE, Hue: ?? }, Fan: { Power: on/off/toggle, Speed: 0-7/INCREASE/DECREASE }}
-    client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":"DECREASE"' + '}}');
+    var client_message = JSON.stringify('{"Name":"' + name + '","IP":"' + ip + '","MAC":"' + mac + '","Model":"' + model + '","Series":"' + series + '","Light": {"Power":"", "Brightness":"", "Hue":"" }' + ',"Fan": {"Power":"", "Speed":"DECREASE"' + '}}');
     // Send request
     socket.emit("devicecontrol", client_message);
     
     return false;                                                               
-};
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 //                          End fan control functions
@@ -177,9 +176,9 @@ function deviceFanDecrease(name, ip, model, series, mac) {
 //  the sqlite3 database
 ////////////////////////////////////////////////////////////////////////////////
 function databaseUpdateDevices() {
-   client_message = JSON.stringify('{"Command":"update"}');  
+   var client_message = JSON.stringify('{"Command":"update"}');  
    socket.emit('databaseupdatedevices', client_message);    
-};
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 //                          End database update functions
@@ -191,81 +190,57 @@ function databaseUpdateDevices() {
 //  the list needs to be refreshed on screen
 ////////////////////////////////////////////////////////////////////////////////
 function databaseGetListOfDevices() {
-    var table = document.getElementById("DeviceTable");                         // Selects which table by ID
-    var i = 0;                                                                  // Inializes the counter to 0   
-    // Clear table before refreshing it
-    for ( var q = table.rows.length - 1; q > 0; q--) {
-        table.deleteRow(q);
-    }
-    $.getJSON('getdevicelist', function(data) {
-        for ( var key in data ) {                                               // Starts filling in the rest of the table based on the JSON data
-            row = table.insertRow(i+1);
-            cellName = row.insertCell(i);
-            cellIP = row.insertCell(i+1);
-            cellModel = row.insertCell(i+2);
-            cellSeries = row.insertCell(i+3);
-            cellMAC = row.insertCell(i+4);
-            cellName.innerHTML = data[key]["Name"];
-            cellIP.innerHTML = data[key]["IP"];
-            cellModel.innerHTML = data[key]["Model"];
-            cellSeries.innerHTML = data[key]["Series"];
-            cellMAC.innerHTML = data[key]["MAC"];
-            //console.log(cellName.innerHTML);
-            deviceName = cellName.innerHTML;
-            deviceIP = cellIP.innerHTML;
-            deviceModel = cellModel.innerHTML;
-            deviceSeries = cellSeries.innerHTML;
-            deviceMAC = cellMAC.innerHTML;
-            row.setAttribute('onclick', 'deviceClickedInTable("' + deviceName +'", "' + deviceIP + '", "' + deviceModel + '", "' + deviceSeries + '", "' + deviceMAC + '")' );       // Sets the row onclick attribute to pass into the function to save the data
-        }         
-    });  
-};
+    // Start the section that loads the other table, this should be a much better loader than what I wrote
+     $(document).ready(function(e) {
+        $.getJSON('getdevicelist', function(data) {
+            var data1 = [];
+            for ( var key in data ) {
+               data1.push( {name: data[key].Name, ip: data[key].IP, model: data[key].Model, series: data[key].Series, mac: data[key].MAC} );
+               //console.log(data1);
+           }
+            var dt = dynamicTable.config('data-table', 
+                        ['name', 'ip', 'model', 'series', 'mac'], 
+                        ['Name', 'IP Address', 'Model', 'Series', 'Mac Address'], //set to null for field names instead of custom header names
+                        'There are no items to list...');
+            dt.load(data1);
+        
+            
+        });
+    });
+    
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 //                          End database get list of devices functions
 ////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////////////////////////////////////////////
-//                 Start webpage table setup
-//  This fuction just sets up the device table
+//                          Start websocket setup
 ////////////////////////////////////////////////////////////////////////////////
-function setupDeviceTable() {
-    var table = document.getElementById("DeviceTable");                             // Selects which table by ID
-    var i = 0;                                                                      // Inializes the counter to 0
-    var header = table.createTHead();                                               // Sets up the header of the columns
-    var row = header.insertRow(i);                                                  // Inserts header row
-    var cellName = row.insertCell(i);                                               // Inserts the 1st cell
-    var cellIP = row.insertCell(i+1);                                               // Inserts 2nd cell
-    var cellModel = row.insertCell(i+2);                                            // etc
-    var cellSeries = row.insertCell(i+3);                   
-    var cellMAC = row.insertCell(i+4);
-    cellName.innerHTML = "<b>Name</b>";                                             // Makes the 1st row a bunch of bold text
-    cellIP.innerHTML = "<b>IP Address</b>";
-    cellModel.innerHTML = "<b>Model</b>";
-    cellSeries.innerHTML = "<b>Series</b>";
-    cellMAC.innerHTML = "<b>MAC Address</b>";
-    table.style.border = "thin solid black";
-    table.style.backgroundColor='#BCD4EC';                                                                  // Inializes the counter to 0
-};
+
+function setupWebsocket() {
+    // Set up the receiving of the websocket as soon as possible
+    var WEB_SOCKET_SWF_LOCATION = "/static/WebSocketMain.swf";
+    var WEB_SOCKET_DEBUG = true;
+    var socket = io.connect('http://' + document.domain + ':' + location.port);
+}
 
 ////////////////////////////////////////////////////////////////////////////////
-//                          End database get list of devices functions
+//                          End websocket setup
 ////////////////////////////////////////////////////////////////////////////////
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // This section will be for javascript that constantly runs once page is loaded, such as device status
 
-// Set up the receiving of the websocket as soon as possible
-WEB_SOCKET_SWF_LOCATION = "/static/WebSocketMain.swf";
-WEB_SOCKET_DEBUG = true;
-socket = io.connect('http://' + document.domain + ':' + location.port);
-
 // Used to stop the previous setInterval so I am not spamming the devices
 var refreshIntervalID;
+var WEB_SOCKET_SWF_LOCATION = "/static/WebSocketMain.swf";
+var WEB_SOCKET_DEBUG = true;
+var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-function displayStatus() {
+//function displayStatus() {
     socket.on('devicestatus', function(message) {
-        //console.log("[x] Received    : " + message);
+        console.log("[x] Received    : " + message);
         var status = JSON.stringify(message);
         //console.log("Message Data: " + message.light.status);
         
@@ -278,15 +253,21 @@ function displayStatus() {
         $jqFanStatus.html(message.fan.status);                                  // Overwrites the HTML text with the value of the variable
         $jqFanSpeed.html(message.fan.speed);                                    // Overwrites the HTML text with the value of the variable
     });
-};
+//}
 
-window.onload = displayStatus;
-window.onload = setupDeviceTable();
-//window.onload = databaseGetListOfDevices();
+//window.onload = displayStatus();
 // Refresh the device list every 1 seconds
 window.setInterval(databaseGetListOfDevices, 1000);
 
-
+// Code that runs constantly. Needed to identify what the IP address is of the row that was clicked
+$("#data-table").on("click", "tr", function(e) {
+    var name = $(this).find('td:eq(0)').text();
+    var ip = $(this).find('td:eq(1)').text();
+    var model = $(this).find('td:eq(2)').text();
+    var series = $(this).find('td:eq(3)').text();
+    var mac = $(this).find('td:eq(4)').text();
+    deviceClickedInTable(name, ip, model, series, mac);
+});
 
 
 
